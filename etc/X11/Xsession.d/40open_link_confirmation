794215a6d37ef54ab8b4d3026009064a7af75ce0
Patrick Schleizer <adrelanos@riseup.net>
2014-07-03 14:01:46 +0000

    fix

---
+++ etc/X11/Xsession.d/40open_link_confirmation
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+## This file is part of Whonix.
+## Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
+## See the file COPYING for copying conditions.
+
+if [ -x /usr/lib/open-link-confirmation/open-link-confirmation.sh ]; then
+   . /usr/lib/open-link-confirmation/open-link-confirmation.sh
+fi
---
+++ usr/lib/open-link-confirmation/open-link-confirmation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## This file is part of Whonix.
+## Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
+## See the file COPYING for copying conditions.
+
+## See also:
+## The Default Browser on Linux Debacle
+## http://blog.codef00.com/2011/02/18/the-default-browser-on-linux-debacle/
+export BROWSER="/usr/lib/open_link_confirmation"

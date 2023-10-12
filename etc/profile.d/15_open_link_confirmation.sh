82f54003a4b429cf5f14a38df2809826ad6f25e0
Patrick Schleizer <adrelanos@whonix.org>
2023-10-12 10:40:27 -0400

    refactoring environment variables loading mechanism

--- etc/profile.d/15_open_link_confirmation.sh
+++ etc/profile.d/15_open_link_confirmation.sh
@@ -3,6 +3,6 @@
 ## Copyright (C) 2012 - 2023 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
-if [ -x /usr/lib/open-link-confirmation/open-link-confirmation.sh ]; then
-   . /usr/lib/open-link-confirmation/open-link-confirmation.sh
+if [ -x /usr/libexec/open-link-confirmation/open-link-confirmation.sh ]; then
+   . /usr/libexec/open-link-confirmation/open-link-confirmation.sh
 fi

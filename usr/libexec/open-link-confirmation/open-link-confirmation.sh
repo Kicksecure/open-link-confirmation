819e0aabcf9881078dc63ab35a604b603820ab2d
Patrick Schleizer <adrelanos@whonix.org>
2025-05-21 08:35:54 -0400

    fix

--- usr/libexec/open-link-confirmation/open-link-confirmation.sh
+++ usr/libexec/open-link-confirmation/open-link-confirmation.sh
@@ -9,14 +9,14 @@
 export BROWSER="/usr/libexec/open-link-confirmation/open-link-confirmation"
 
 if [ -z "$XDG_CONFIG_DIRS" ]; then
-   XDG_CONFIG_DIRS=/etc/xdg
+   XDG_CONFIG_DIRS="/etc/xdg"
 fi
 if [ -z "$XDG_DATA_DIRS" ]; then
-   XDG_DATA_DIRS=/usr/local/share/:/usr/share/
+   XDG_DATA_DIRS="/usr/local/share/:/usr/share/"
 fi
-if ! echo "$XDG_CONFIG_DIRS" | grep --quiet /usr/share/open-link-confirmation/ ; then
-   export XDG_CONFIG_DIRS=/usr/share/open-link-confirmation/:$XDG_CONFIG_DIRS
+if ! printf '%s\n' "$XDG_CONFIG_DIRS" | grep -- "/usr/share/open-link-confirmation/" >/dev/null 2>/dev/null; then
+   export XDG_CONFIG_DIRS="/usr/share/open-link-confirmation/:$XDG_CONFIG_DIRS"
 fi
-if ! echo "$XDG_DATA_DIRS" | grep --quiet /usr/share/open-link-confirmation/xdg-override/ ; then
-   export XDG_DATA_DIRS=/usr/share/open-link-confirmation/xdg-override/:$XDG_DATA_DIRS
+if ! printf '%s\n' "$XDG_DATA_DIRS" | grep -- /usr/share/open-link-confirmation/xdg-override/ >/dev/null 2>/dev/null; then
+   export XDG_DATA_DIRS="/usr/share/open-link-confirmation/xdg-override/:$XDG_DATA_DIRS"
 fi

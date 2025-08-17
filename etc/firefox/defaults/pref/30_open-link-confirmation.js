a23b1f7eb5def6b85e5570dd0a93dc531cb2ac67
Aaron Rainbolt <arraybolt3@ubuntu.com>
2025-08-17 15:43:53 -0500

    Configure Firefox to not ask to be the default browser

---
+++ etc/firefox/defaults/pref/30_open-link-confirmation.js
@@ -0,0 +1,5 @@
+// Copyright (C) 2012 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
+// See the file COPYING for copying conditions.
+
+// Disable default browser requests to avoid breaking open-link-confirmation.
+pref("browser.shell.checkDefaultBrowser", false);
